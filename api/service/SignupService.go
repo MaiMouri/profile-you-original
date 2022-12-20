1f2722c7409a70abc2aac8890084dd6ab160eab7
“MouriMai” <mori-ma@ibjapan.jp>
2022-12-20 18:57:36 +0900

    login and signup not yet

--- api/service/SignupService.go
+++ api/service/SignupService.go
@@ -29,7 +29,7 @@ func Register_proc(email string, password string) string {
 	fmt.Fprintf(os.Stderr, "*** Register_proc ***\n")
 
 	now := time.Now()
-	str_out, _ := Generate_token_proc(email, now)
+	str_out, _ := GenerateToken(email, now)
 	db := sqlite.New()
 	// db, _ := gorm.Open("mysql", "scott:tiger123@/test_db")
 	connect, err := db.DB()
@@ -38,7 +38,7 @@ func Register_proc(email string, password string) string {
 	}
 	defer connect.Close()
 
-	sql_str := "insert into users (email,password,created) values (?,?,?,?,?)"
+	sql_str := "insert into users (email,password,created) values (?,?,?)"
 	db.Exec(sql_str, email, password, now)
 
 	return str_out
